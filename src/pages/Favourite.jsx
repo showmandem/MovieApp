7b0e86a2ae83aec4c78e707387d460c2b8dfad40
Derrick Owusu-Asare <aderrick649@gmail.com>
2025-12-31 14:19:38 +0000

    altered a code

--- src/pages/Favourite.jsx
+++ src/pages/Favourite.jsx
@@ -15,7 +15,6 @@ const Favourite = () => {
   return (
     <div className="favourite-container">
       <div className="favourite-empty-state">
-        <div className="favourite-emoji">🎭</div>
         <h2 className="favourite-title">You have not added a favourite yet</h2>
         <p className="favourite-text">Start adding movies</p>
       </div>
